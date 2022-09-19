191ce4f8a894e3d573eadfddd81511d30f83688f
Alfonso Martínez Piñango <alfonsoiniesta35@hotmail.es>
2022-09-19 13:00:24 +0200

    Primeros pasos

---
+++ modelo.js
@@ -0,0 +1,17 @@
+
+//CREACIÓN DEL JUEGO
+
+function Juego(){
+    this.partidas=[];
+    this.agregarPartida=function(nombre){
+        this.partidas.push(new Partida(nombre))
+    }
+
+}
+
+
+//CREACIÓN DE LA PARTIDA
+function Partida(){
+
+
+}
