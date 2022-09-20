c56f9aba4e2616534db17f724815184f467d36bf
Alfonso Martínez Piñango <alfonsoiniesta35@hotmail.es>
2022-09-20 09:38:59 +0200

    Clase 20-09: Modificación del modelo

--- modelo.js
+++ modelo.js
@@ -1,10 +1,32 @@
 
 //CREACIÓN DEL JUEGO
-
 function Juego(){
-    this.partidas=[];
-    this.agregarPartida=function(nombre){
-        this.partidas.push(new Partida(nombre))
+    this.partidas={}; //Diccionario (asociación clave-valor) this.partidas=[];
+    this.usuarios={}; //array asociativo
+
+    this.agregarUsuario=function(nick){
+        if (!this.usuarios[nick]){
+            this.usuarios[nick]=new Usuario(nick,this);
+        }
+    }
+
+    this.crearPartida=function(nick){
+        //obtener código único
+        //crear la partida con propietario nick
+        //devolver el código
+        console.log("partida creada");
+    }
+
+    
+}
+
+//CREACIÓN DE UN USUARIO
+function Usuario(nick,juego){
+    this.nick=nick;
+    this.juego=juego;  //Usuario conoce la clase Juego
+
+    this.crearPartida=function(){
+        this.juego.crearPartida(this.nick)
     }
 
 }
@@ -12,6 +34,7 @@ function Juego(){
 
 //CREACIÓN DE LA PARTIDA
 function Partida(){
+    this.codigo;
 
 
 }
